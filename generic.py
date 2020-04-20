1441d4dd91bc70e858a9fa53e3efb58c31968c81
Akshay Uppal <akshayuppal3@gmail.com>
2020-04-20 16:07:54 -0500

    adding zip func

--- generic.py
+++ generic.py
@@ -12,4 +12,25 @@ def clear_dir_contents(dir: Path):
             else:
                 shutil.rmtree(filename)
     except Exception as e:
-        print(f"Unable to delete reason: {e}")
+        print(f"Unable to delete reason: {e}")=
+        
+        
+def get_model_json(model_meta_path: Path) -> dict:
+    with model_meta_path.open() as json_file:
+        model_json = json.load(json_file)
+    return model_json
+
+
+def zip_files(name_list: List[str], file_list: List[pd.DataFrame]) -> bytes:
+    outfile = io.BytesIO()
+    with zipfile.ZipFile(outfile, 'w') as zf:
+        for name, data in zip(name_list, file_list):
+            if name.endswith('.csv'):
+                zf.writestr(name, data.to_csv())
+            else:
+                zf.writestr(name, data)
+    return outfile.getvalue()
+
+def unzip(path_to_zip_file: Path, destination_dir: Path):
+    with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
+        zip_ref.extractall(destination_dir)
